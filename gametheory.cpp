509181c46c24baf8a30e69c2da52d73d548e37e4
Alan69359 <alan69359@gmail.com>
2023-04-27 14:24:19 +0800

    commit

--- gametheory.cpp
+++ gametheory.cpp
@@ -8,7 +8,8 @@ typedef pair<int,int> pii;
 const int N=1e5+10,M=1e5+10,INF=0x3f3f3f3f,MOD=1e9+7;
 
 int n,m;
-int a[N];
+int f[N];
+int set0[N];
 
 int nim1(){
     scanf("%d",&n);
@@ -29,6 +30,39 @@ int nim2(){
         int a;
         scanf("%d",&a);
         if(i&1)res^=a;
+    }
+    if(res)return 0;
+    else return 1;
+}
+
+int sg(int x){
+    if(f[x]!=-1)return f[x];
+    unordered_set<int>s;
+    for(int i=0;i<m;i++){
+        int co=set0[i];
+        if(x>=co){
+            s.insert(sg(x-co));
+        }
+    }
+    for(int i=0;;i++){
+        if(!s.count(i)){
+            return f[x]=i;
+        }
+    }
+}
+
+int nim3(){
+    scanf("%d",&m);
+    for(int i=1;i<=m;i++){
+        scanf("%d",&set0[i]);
+    }
+    scanf("%d",&n);
+    memset(f,-1,sizeof f);
+    int res=0;
+    for(int i=0;i<n;i++){
+        int a;
+        scanf("%d",&a);
+        res^=sg(a);
     }
     if(res)return 0;
     else return 1;
