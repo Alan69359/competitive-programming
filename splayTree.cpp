d2028b0641ca2786016f9a339c439fba4d9739d0
Alan69359 <alan69359@gmail.com>
2023-05-30 16:07:31 +0800

    commit

--- splayTree.cpp
+++ splayTree.cpp
@@ -9,7 +9,7 @@ const int N=1e6+10,M=1e6,INF=0x3f3f3f3f,MOD=1e9+7;
 
 int n,m;
 int root,idx;
-int pa[N];
+int p[N];
 int root2[N];
 
 struct Node{
@@ -145,8 +145,8 @@ void rem(int id,int x){
 }
 
 int find(int x){
-    if(pa[x]==x)return x;
-    else return pa[x]=find(pa[x]);
+    if(p[x]==x)return x;
+    else return p[x]=find(p[x]);
 }
 
 void dfs(int u,int id){
@@ -166,7 +166,7 @@ void mer(int id1,int id2){
             swap(a,b);
         }
         dfs(root2[a],b);
-        pa[a]=b;
+        p[a]=b;
     }
 }
 
