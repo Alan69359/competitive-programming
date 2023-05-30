fd5aa43f24093efa5529a0c1973ffb49cd4dea6f
Alan <50506748+Alan69359@users.noreply.github.com>
2023-05-30 16:48:00 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,106 +1,76 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-const int N=100010;
-
-int n,m;
-
-struct Node{
-    int s[2],p,w;
-    int sum,lazy;
-}tr[N];
-int stk[N];
-
-void pushrev(int x){
-    swap(tr[x].s[0],tr[x].s[1]);
-    tr[x].lazy^=1;
-}
-void pushup(int x){
-    tr[x].sum=tr[tr[x].s[0]].sum^tr[x].w^tr[tr[x].s[1]].sum;
-}
-
-void pushdown(int x){
-    if(tr[x].lazy){
-        pushrev(tr[x].s[0]),pushrev(tr[x].s[1]);
-        tr[x].lazy=0;
+void dfs1(int u, int father, int dep)
+{
+    depth[u] = dep, fa[u] = father, trsize[u] = 1;
+    for (int i = he[u]; ~i; i = ne[i])
+    {
+        int j = no[i];
+        if (j == father) continue;
+        dfs1(j, u, dep + 1);
+        trsize[u] += trsize[j];
+        if (trsize[son[u]] < trsize[j]) son[u] = j;
     }
 }
 
-bool isroot(int x){
-    return tr[tr[x].p].s[0]!=x&&tr[tr[x].p].s[1]!=x;
-}
-
-void rotate(int x){
-    int y=tr[x].p,z=tr[y].p;
-    int k=tr[y].s[1]==x;
-    if(!isroot(y))tr[z].s[tr[z].s[1]==y]=x;
-    tr[x].p=z;
-    tr[y].s[k]=tr[x].s[k^1],tr[tr[x].s[k^1]].p=y;
-    tr[x].s[k^1]=y,tr[y].p=x;
-    pushup(y),pushup(x);
-}
-
-void splay(int x){
-    int top=0,r=x;
-    stk[++top]=r;
-    while(!isroot(r)){
-        stk[++top]=r=tr[r].p;
-    }
-    while(top){
-        pushdown(stk[top--]);
-    }
-    while(!isroot(x)){
-        int y=tr[x].p,z=tr[y].p;
-        if(!isroot(y)){
-            if((tr[y].s[1]==x)^(tr[z].s[1]==y))rotate(x);
-            else rotate(y);
-        }
-        rotate(x);
+void dfs2(int u, int par)
+{
+    id[u] = ++ cnt, nwe[cnt] = we[u], p[u] = par;
+    if (!son[u]) return;
+    dfs2(son[u], par);
+    for (int i = he[u]; ~i; i = ne[i])
+    {
+        int j = no[i];
+        if (j == fa[u] || j == son[u]) continue;
+        dfs2(j, j);
     }
 }
 
-void access(int x){
-    int z=x;
-    for(int y=0;x;y=x,x=tr[x].p){
-        splay(x);
-        tr[x].s[1]=y;
-        pushup(x);
+void updatepath(int u, int v, int k)
+{
+    while (p[u] != p[v])
+    {
+        if (depth[p[u]] < depth[p[v]]) swap(u, v);
+        modifyseq(1, id[p[u]], id[u], k);
+        u = fa[p[u]];
     }
-    splay(z);
+    if (depth[u] < depth[v]) swap(u, v);
+    modifyseq(1, id[v], id[u], k);
 }
 
-void makeroot(int x){
-    access(x);
-    pushrev(x);
-}
-
-int findroot(int x){
-    access(x);
-    while(tr[x].s[0]){
-        pushdown(x);
-        x=tr[x].s[0];
+ll querypath(int u, int v)
+{
+    ll res = 0;
+    while (p[u] != p[v])
+    {
+        if (depth[p[u]] < depth[p[v]]) swap(u, v);
+        res += querysum(1, id[p[u]], id[u]);
+        u = fa[p[u]];
     }
-    splay(x);
-    return x;
+    if (depth[u] < depth[v]) swap(u, v);
+    res += querysum(1, id[v], id[u]);
+    return res;
 }
 
-void split(int x,int y){
-    makeroot(x);
-    access(y);
+void updatetree(int u, int k)
+{
+    modifyseq(1, id[u], id[u] + trsize[u] - 1, k);
 }
 
-void link(int x,int y){
-    makeroot(x);
-    if(findroot(y)!=x){
-        tr[x].p=y;
+ll querytree(int u)
+{
+    return querysum(1, id[u], id[u] + trsize[u] - 1);
+}
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i ++ ) scanf("%d", &we[i]);
+    memset(he, -1, sizeof he);
+    for (int i = 0; i < n - 1; i ++ )
+    {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        add(a, b), add(b, a);
     }
-}
-
-void cut(int x,int y){
-    makeroot(x);
-    if(findroot(y)==x&&tr[y].p==x&&!tr[y].s[0]){
-        tr[x].s[1]=tr[y].p=0;
-        pushup(x);
-    }
+    dfs1(1, -1, 1);
+    dfs2(1, 1);   
 }
